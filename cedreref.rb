c260801f31e16fa3341d637e00c98e03b9a66fb2
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-26 23:55:44 +0000

    Create cedreref.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ cedreref.rb
@@ -0,0 +1,5 @@
+require "QuiQuandQuoi/Cedreref.rb"
+
+cadreref("My favorite food is curries.",   "Relationship: Food", 0.33,
+         "My favorite food is spaghetti.", "Relationship: Food", 0.33,
+         "My favorite pet is a dog.",      "Relationship: Pets", 0.33)
