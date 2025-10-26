5ee809cb4410c0ab8c145a8422c51afc2205dde3
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-26 23:57:46 +0000

    Update cedreref.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- cedreref.rb
+++ cedreref.rb
@@ -3,3 +3,7 @@ require "QuiQuandQuoi/Cedreref.rb"
 cadreref("My favorite food is curries.",   "Relationship: Food", 0.33,
          "My favorite food is spaghetti.", "Relationship: Food", 0.33,
          "My favorite pet is a dog.",      "Relationship: Pets", 0.33)
+
+cadreref("My favourite outfit is haute couture.",     "Relationship: Fashion", 0.33,
+         "My favourite outfit is historical French.", "Relationship: Fashion", 0.33,
+         "My favorite movie is 12 Monkeys.",          "Relationship: Movies",  0.33)
