54f0876bb20dba9fd73f604ca9af88f4204de667
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-26 09:43:36 +0000

    Create test.rb

---
+++ test.rb
@@ -0,0 +1,22 @@
+require "NeoPathfinding"
+
+a = cette("pomme", "rouge")
+b = maisette("pomme", "vert")
+c = sinon("pomme", "rouge", "vert)
+
+gabbler = Gabbler.new
+
+2.times do
+  get_statistics(:cette,    a,
+                 :maisette, b,
+                 :sinon,    c)
+
+                 d = dynamic_reward_allocation
+end
+
+e = gabbler.new(d)
+
+print e
+puts " "
+
+gabbler.unlearn
