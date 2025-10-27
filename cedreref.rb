d9a334f9ad6ce406c8cd909c49c4b2c771bd9cea
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-27 00:04:14 +0000

    Update cedreref.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- cedreref.rb
+++ cedreref.rb
@@ -7,3 +7,7 @@ cadreref("My favorite food is curries.",   "Relationship: Food", 0.33,
 cadreref("My favourite outfit is haute couture.",     "Relationship: Fashion", 0.33,
          "My favourite outfit is historical French.", "Relationship: Fashion", 0.33,
          "My favorite movie is 12 Monkeys.",          "Relationship: Movies",  0.33)
+
+cadreref(cette("pomme", "rouge"),         "Relationship: Fruit", 0.33,
+         maisette("pomme", "vert"),       "Relationship: Fruit", 0.33,
+         sinon("pomme", "rouge", "vert"), "Relationship: Fruit", 0.33)
