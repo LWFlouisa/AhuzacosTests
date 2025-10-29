053eb327211462bdfd01756c4ad337b52513ad27
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-29 08:37:15 +0000

    Create quandquoicomme.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ quandquoicomme.rb
@@ -0,0 +1,11 @@
+require "NeoPathfinding"
+
+quandquoicomme("Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",
+               "A black cat",               "A black crow",              "A black rat",               "A white dove",              "A white turkey",
+               "climbed a wall.",           "flew onto a sign.",         "crawled into a hole.",      "sang the song of peace.",   "is eaten for thanksgiving.",
+               "That cat is black",         "That bird is black",        "That mouse is black",       "That dove is white.",       "That turkey is cooked.",
+               "But that turkey is white.", "But that dove is white.",   "But that turkey is white.", "But that cat is black.",    "But that crow is black.",
+               "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.")
+              
+               print $current_case
+               puts " "
