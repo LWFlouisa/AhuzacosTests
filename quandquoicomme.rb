e2edd8e3633385000e76ad01b770e00b863bd84f
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-10-29 11:12:13 +0000

    Update quandquoicomme.rb
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

--- quandquoicomme.rb
+++ quandquoicomme.rb
@@ -1,11 +1,66 @@
 require "NeoPathfinding"
 
-quandquoicomme("Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",
-               "A black cat",               "A black crow",              "A black rat",               "A white dove",              "A white turkey",
-               "climbed a wall.",           "flew onto a sign.",         "crawled into a hole.",      "sang the song of peace.",   "is eaten for thanksgiving.",
-               "That cat is black",         "That bird is black",        "That mouse is black",       "That dove is white.",       "That turkey is cooked.",
-               "But that turkey is white.", "But that dove is white.",   "But that turkey is white.", "But that cat is black.",    "But that crow is black.",
-               "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.")
+#quandquoicomme("Around 6:00 A.M.",         "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",          "Around 6:00 A.M.",
+#              "A black cat",               "A black crow",              "A black rat",               "A white dove",              "A white turkey",
+#              "climbed a wall.",           "flew onto a sign.",         "crawled into a hole.",      "sang the song of peace.",   "is eaten for thanksgiving.",
+#              "That cat is black",         "That bird is black",        "That mouse is black",       "That dove is white.",       "That turkey is cooked.",
+#              "But that turkey is white.", "But that dove is white.",   "But that turkey is white.", "But that cat is black.",    "But that crow is black.",
+#              "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.", "Otherwise both are white.")
               
-               print $current_case
-               puts " "
+quandquoicomme("Between midnight to 4 A.M.",   "Normal church going hours",    "Podcasts at different times",         "Depends on when events happen",      "Around 7:00 P.M.",
+              "Varies from dreamer to next",   "Preists dress in robes",       "Lecturers in podcasts",               "Commitee of specialists",            "Anomalous dolls of different types",
+              "blend in from scene to scene",  "Calls believers into room",    "Presents ufos and related",           "Coincides with traumatic events",    "Tends to wait on bookselfs or hovers",
+              "That dream is ambiguous",       "That preist is in black",      "That talkshow hosts changes outfits", "That leader dresses formally",       "That doll wears victorian clothes.",
+              "But that dream is normal.",     "But that preist is in white.", "But the guest is wearing all black.", "But others wear military uniforms.", "But that one wears barbie outfits.",
+              "Otherwise dream is nightmare.", "Otherwise preists are nude.",  "Othwerise both wear all black.",      "Otherwise both dress casually.",     "Otherwise both wear modern clothes.")
+              
+#quandquoicomme("Around 6:00 A.M.",               "Around 6:00 A.M.",                "Around 6:00 A.M.",                "Around 6:00 A.M.",               "Around 6:00 A.M.",
+#              "A black cat",                     "A black crow",                    "A black rat",                     "A white dove",                    "A white turkey",
+#              "climbed a wall.",                 "flew onto a sign.",               "crawled into a hole.",            "sang the song of peace.",         "is eaten for thanksgiving.",
+#              cette("chatte", "noir"),           cette("oiseau", "noir"),           cette("souris", "noir"),           cette("pidgeon", "balnc"),         cette("dinde", "flambe"),
+#              maisette("dinde", "blanc"),        maisette("pidgeon", "blanc"),      maisette("dinde", "blanc"),        maisette("chatte", "noir"),        maisette("pidgeon", "blanc"),
+#              sinon("le deux", "noir", "blanc"), sinon("le deux", "noir", "blanc"), sinon("le deux", "noir", "blanc"), sinon("le deux", "noir", "blanc"), sinon("le deux", "noir", "blanc"))
+              
+current_case = $current_case
+size_limit   = current_case.size.to_i
+index        = 0
+
+size_limit.times do
+
+  gabbler = Gabbler.new
+
+  #corpus    = File.read(current_case)
+
+  gabbler.learn(current_case[index])
+
+  print gabbler.sentence; puts " "
+
+  sleep(0.5)
+
+  gabbler.unlearn!
+  
+  index = index + 1
+end
+
+2.times do
+  get_statistics(:when, current_case[0],
+                 :what, current_case[1],
+                 :how,  current_case[2])
+                 
+                 dynamic_reward_allocation
+end
+
+puts "WHEN"
+cadreref("Around 7:00 P.M.",                         "Relationship: Haunted Dolls", 0.165,
+         "Otherwise depends on when events happen.", "Relationship: Dreams",        0.165,
+         "Lecturers in podcasts",                    "Relationship: Conspiracies",  0.165)
+
+puts "WHAT"
+cadreref("Anomalous dolls of different types.", "Relationship: Haunted Dolls", 0.165,
+         "Varies from dreamer to next.",        "Relationship: Dreams",        0.165,
+         "Commitee of specialists",             "Relationship: Psyops",        0.165)
+         
+puts "HOW"
+cadreref("Blend in from scene to scene.",   "Relationship: Haunted Dolls",         0.165,
+         "Coincides with traumatic event.", "Relationship: Conspiracy And Psyops", 0.165,
+         "Podcasts recorded every day.",    "Relationship: Conspiracy And Psyops", 0.165)
